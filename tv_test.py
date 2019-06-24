4faa0605697d3976167d0e3c085b66f1a0ab2026
Derek <derekgloudemans@gmail.com>
2019-06-24 11:27:59 -0500

    Copy some dataloading code from tutorial

--- tv_test.py
+++ tv_test.py
@@ -1,6 +1,9 @@
 # exploratory file for working with torchvision classifiers
-from __future__ import print_function, division
 
+# much of the code is from:
+# https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+
+from __future__ import print_function, division
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -13,4 +16,45 @@ import time
 import os
 import copy
 
+# load pretrained model
 resnet18 = models.resnet18(pretrained = True)
+
+'''All pre-trained models expect input images normalized in the same way, 
+i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), 
+where H and W are expected to be at least 224. The images have to be loaded 
+in to a range of [0, 1] and then normalized using mean = [0.485, 0.456, 0.406] 
+and std = [0.229, 0.224, 0.225]. You can use the following transform to normalize:
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+'''
+
+# so, we need to take any images we'd like to classify and load them batchwise as (Bx3xHxW) tensors
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(600),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(620),
+        transforms.CenterCrop(600),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'stanford_cars'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
