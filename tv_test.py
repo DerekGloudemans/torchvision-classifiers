5989e6d7ce51ea9dd587fdd902cf06ae5002617e
Derek <derekgloudemans@gmail.com>
2019-06-24 11:06:26 -0500

    Add initial file for working through use of classifier

---
+++ tv_test.py
@@ -0,0 +1,16 @@
+# exploratory file for working with torchvision classifiers
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+resnet18 = models.resnet18(pretrained = True)
