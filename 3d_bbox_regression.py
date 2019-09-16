9349d931d64633a9387e1a4aa95ab215c2b87f2c
Derek Gloudemans <35013478+DerekGloudemans@users.noreply.github.com>
2019-09-16 09:57:21 -0500

    Add PackageNet and training loop

--- 3d_bbox_regression.py
+++ 3d_bbox_regression.py
@@ -26,6 +26,8 @@ examples to tensors. Transforms include:
 
 # this seems to be a popular thing to do so I've done it here
 from __future__ import print_function, division
+from parallel_regression_classification import load_model
+from PackageNet import PackageNet
 
 # torch and specific torch packages for convenience
 import torch
@@ -323,6 +325,9 @@ class Test_Dataset_3D(data.Dataset):
         cls = y['class']
             
         cls = self.class_dict[cls.lower()]
+        temp = np.zeros([9])
+        temp[cls] = 1
+        cls = torch.from_numpy(temp).float()
         
         # transform both image and label (note that the 2d and 3d bbox coords must both be scaled)
         im,bbox_2d,bbox_3d = self.scale_crop(im,bbox_2d,bbox_3d,imsize = 224)
@@ -462,12 +467,138 @@ class Test_Dataset_3D(data.Dataset):
         plt.imshow(new_im)
 
 
+
+
+def train_model(model, cls_criterion,reg_criterion, optimizer, scheduler, 
+                dataloaders,dataset_sizes, num_epochs=5, start_epoch = 0):
+    """
+    Alternates between a training step and a validation step at each epoch. 
+    Validation results are reported but don't impact model weights
+    """
+    start = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    for epoch in range(start_epoch,num_epochs):
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                if True: #disable for Adam
+                    scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            count = 0
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                reg_target = labels[2].to(device)
+                cls_target = labels[0].long().to(device)
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    cls_outputs, reg_outputs = model(inputs)
+                    
+#                    # make copy of reg_outputs and zero if target is 0
+#                    # so that bboxes are only learned for positive examples
+#                    temp = cls_target.unsqueeze(1)
+#                    mask = torch.cat((temp,temp,temp,temp),1).float()
+#                    mask.requires_grad = True
+#                    reg_outputs_mod = torch.mul(reg_outputs,mask)
+#                    
+#                    # note that the classification loss is done using class-wise probs rather 
+#                    # than a single class label?
+                    cls_loss = cls_criterion(cls_outputs,cls_target)
+                    reg_loss = reg_criterion(reg_outputs,reg_target)
+                    
+                    
+                    
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        reg_loss.backward(retain_graph = True)
+                        cls_loss.backward()
+                        optimizer.step()
+          
+                # statistics
+                running_loss += (reg_loss.item()+cls_loss.item()) * inputs.size(0)
+                # here we need to define a function that checks the bbox iou with correct 
+                # still wrong - must be across whole batch
+                
+                # convert into class label rather than probs
+                _,cls_outputs = torch.max(cls_outputs,1)
+                # copy data to cpu and numpy arrays for scoring
+                cls_pred = cls_outputs.data.cpu().numpy()
+                reg_pred = reg_outputs.data.cpu().numpy()
+                actual = labels.numpy()
+                
+                
+#                correct,bbox_acc = score_pred(cls_pred,reg_pred,actual)
+                running_corrects += correct
+    
+                # verbose update
+                count += 1
+                if count % 20 == 0:
+#                    print("on minibatch {} -- correct: {} -- avg bbox iou: {} ".format(count,correct,bbox_acc))
+                    print("cls: {}, reg: {}".format(cls_loss.item(),reg_loss.item()))
+                    
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = float(running_corrects) / dataset_sizes[phase] * dataloaders['train'].batch_size
+            
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                del best_model_wts
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+        
+        if epoch % 3 == 0:
+            # save checkpoint
+            PATH = "splitnet_centered_checkpoint_{}.pt".format(epoch)
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': epoch_loss
+                }, PATH)
+
+    time_elapsed = time.time() - start
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+
+
+
 #------------------------------ Main code here -------------------------------#
 if __name__ == "__main__":
     try:
         torch.multiprocessing.set_start_method('spawn')    
     except:
         pass
+    
+    # define start epoch for consistent labeling if checkpoint is reloaded
+    checkpoint_file = None
+    start_epoch = 0
+    num_epochs = 20
     
     # use this to watch gpu in console            watch -n 2 nvidia-smi
     
@@ -480,7 +611,58 @@ if __name__ == "__main__":
     train_data = Train_Dataset_3D(directory,max_scaling = 1.5)
     test_data = Test_Dataset_3D(directory)
 
-    idx = random.randint(0,len(train_data))
-    X, (cls,bbox_2d,bbox_3d,calib) = train_data[idx]
-    train_data.show(idx,plot_3D = True)
-    test_data.show(idx,plot_3D = True)
+    if False: # test the datasets
+        idx = random.randint(0,len(train_data))
+        X, (cls,bbox_2d,bbox_3d,calib) = train_data[idx]
+        train_data.show(idx,plot_3D = True)
+        test_data.show(idx,plot_3D = True)
+    
+    # create training params
+    params = {'batch_size': 32,
+              'shuffle': True,
+              'num_workers': 0}
+
+    try:
+        trainloader
+        testloader
+    except:
+        trainloader = data.DataLoader(train_data, **params)
+        testloader = data.DataLoader(test_data, **params)
+    print("Got dataloaders.")
+       
+    try:
+        model
+    except:
+        # define CNN model
+        model = PackageNet()
+        model = model.to(device)
+    print("Got model.")
+    
+    # define loss functions
+    reg_criterion = nn.MSELoss()
+    cls_criterion = nn.CrossEntropyLoss()
+    
+    # all parameters are being optimized, not just fc layer
+    #optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.SGD(model.parameters(), lr=0.001,momentum = 0.9)
+    
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+    
+
+    # if checkpoint specified, load model and optimizer weights from checkpoint
+    if checkpoint_file != None:
+        model,optimizer,start_epoch = load_model(checkpoint_file, model, optimizer)
+        #model,_,start_epoch = load_model(checkpoint_file, model, optimizer) # optimizer restarts from scratch
+        print("Checkpoint loaded.")
+            
+    # group dataloaders
+    dataloaders = {"train":trainloader, "val": testloader}
+    datasizes = {"train": len(train_data), "val": len(test_data)}
+    
+    if True:    
+    # train model
+        print("Beginning training.")
+        model = train_model(model, cls_criterion, reg_criterion, optimizer, 
+                            exp_lr_scheduler, dataloaders,datasizes,
+                            num_epochs, start_epoch)
